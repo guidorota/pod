ebb650b5aa77f707ed01262a51c23b2a03c799ea
Guido Rota <guido.rota@gmail.com>
2015-01-23 21:31:08 +0100

    Interface up/down tests

--- net/net_test.c
+++ net/net_test.c
@@ -24,13 +24,33 @@ START_TEST(test_create_delete)
     up0 = net_is_up(NET_VETH0); 
     up1 = net_is_up(NET_VETH1);
 
-    err = net_delete("tveth0");
+    err = net_delete(NET_VETH0);
     ck_assert_int_ge(err, 0);
 
     // postpone this check so that an error on net_is_up won't keep the test
     // case from deleting the veth pair
     ck_assert_int_ge(up0, 0);
     ck_assert_int_ge(up1, 0);
+}
+END_TEST
+
+START_TEST(test_up_down)
+{
+    int err;
+
+    err = net_create_veth(NET_VETH0, NET_VETH1);
+    ck_assert_int_ge(err, 0);
+
+    ck_assert_int_eq(net_is_up(NET_VETH0), 0);
+    ck_assert_int_ge(net_up(NET_VETH0), 0);
+    ck_assert_int_eq(net_is_up(NET_VETH0), 1);
+    ck_assert_int_eq(net_is_up(NET_VETH1), 0);
+
+    ck_assert_int_ge(net_down(NET_VETH0), 0);
+    ck_assert_int_eq(net_is_up(NET_VETH0), 0);
+    ck_assert_int_eq(net_is_up(NET_VETH1), 0);
+
+    ck_assert_int_ge(net_delete(NET_VETH0), 0);
 }
 END_TEST
 
@@ -47,6 +67,10 @@ Suite *net_test_suite()
 
     c = tcase_create("test_create_delete");
     tcase_add_test(c, test_create_delete);
+    suite_add_tcase(s, c);
+
+    c = tcase_create("test_up_down");
+    tcase_add_test(c, test_up_down);
     suite_add_tcase(s, c);
 
     return s;
