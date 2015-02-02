dd89f1317a5ef3063da19fa29b1ea4260a4bacc2
Guido Rota <guido.rota@gmail.com>
2015-02-02 17:20:44 +0100

    rtnetlink implementation

--- net/rtnetlink/link.go
+++ net/rtnetlink/link.go
@@ -1,9 +1,10 @@
 package rtnetlink
 
 import (
-	_ "syscall"
+	"syscall"
+	"unsafe"
 
-	_ "github.com/guidorota/pod/net/netlink"
+	"github.com/guidorota/pod/net/netlink"
 )
 
 type Ifinfomsg struct {
@@ -12,5 +13,35 @@ type Ifinfomsg struct {
 	Index  int32
 	Flags  uint32
 	Change uint32
-	Atts   []Rtattr
+}
+
+type LinkInfo struct {
+	Ifi  Ifinfomsg
+	Atts []Attribute
+}
+
+func (l *LinkInfo) encode() []byte {
+	b := make([]byte, 16)
+	l.Ifi.Change = 0xFFFFFFFF
+
+	*(*Ifinfomsg)(unsafe.Pointer(&b[0])) = l.Ifi
+
+	for _, a := range l.Atts {
+		b = append(b, a.Encode()...)
+	}
+
+	return b
+}
+
+func GetAllInfo() ([]LinkInfo, error) {
+	req := &netlink.Message{}
+	req.Type = syscall.RTM_GETLINK
+	req.Flags = syscall.NLM_F_REQUEST | syscall.NLM_F_DUMP
+
+	_, err := request(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
 }
